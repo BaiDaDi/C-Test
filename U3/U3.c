73a5d59e135dd464568ef76b6680f8fa18aa1667
BaiDaDi <55625327+BaiDaDi@users.noreply.github.com>
2021-06-12 11:47:33 +0800

    U3

---
+++ U3/U3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int i,year,high;
+float j,cup;
+double box,num;
+
+
+int main(void)
+{
+/*	printf("\n"); printf("\n"); printf("2:\n");
+	scanf_s("%d", &i);
+	printf("%d %c", i,i);
+
+	printf("\n"); printf("\n"); printf("3:\n");
+	printf("\a");
+	printf("Fuck_MIMA");
+
+	printf("\n"); printf("\n"); printf("4:\n");
+	scanf_s("%f", &j);
+	printf("%f\n%e\n%p\n",j,j,j);
+
+	printf("\n"); printf("\n"); printf("5:\n");
+	scanf_s("%d", &year);
+	printf("%.1lf", year*3.156e7);
+
+	printf("\n"); printf("\n"); printf("6:\n");
+	scanf_s("%d", &num);
+	box = num * 950/3e-23 ;
+	printf("%d", box);
+
+	printf("\n"); printf("\n"); printf("7:\n");
+	scanf_s("%i", &high);
+	printf("%f", high * 2.54);
+
+	printf("\n"); printf("\n"); printf("8:\n");
+	scanf_s("%f", &cup);
+	printf("1:%.6f;\n2:%.0f;\n3:%.6f;\n4:%.6f;\n5:%.6f;\n",
+		  cup / 2, cup, cup * 8, cup * 8 * 2, cup * 8 * 2 * 3);
+*/
+	return 0;
+}
