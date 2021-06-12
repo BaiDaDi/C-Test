507cd29d022ad403b4f2b69661de147ebf76ce50
BaiDaDi <55625327+BaiDaDi@users.noreply.github.com>
2021-06-12 11:48:26 +0800

    U4

---
+++ U4/1/U4main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#define BOOK "War And Peace"
+int main(void)
+{
+	char name1[10] = {0};
+	char name2[10] = {0};
+	float i,j,k;
+
+/*	printf("\n"); printf("\n"); printf("1:\n");
+	
+	scanf_s("%s", name1,50);
+	scanf_s("%s", name2, 50);
+	printf("%s.%s\n", name1, name2);
+
+	printf("\n"); printf("\n"); printf("2:\n");
+
+	scanf_s("%s", name1, 10);
+	scanf_s("%s", name2, 10);
+	printf("1- \"  %s. %s \" \n", name1, name2);
+	printf("2- \"  %10s. %10s \" \n", name1, name2);
+	printf("3- \"  %-10s. %-10s \" \n", name1, name2);
+	printf("4- \"  %-13s. %-13s \" \n", name1, name2);
+
+	printf("\n"); printf("\n"); printf("3:\n");
+	scanf_s("%f", &i);
+	printf("1-%.2f\n", i);
+	printf("2-%.2e\n", i);
+
+	printf("\n"); printf("\n"); printf("4:\n");
+	scanf_s("%s", name1, 10);
+	scanf_s("%s", name2, 10);
+	scanf_s("%f", &i);
+	printf("%s%s,you are %.0f feet tall\n", name1, name2,i);
+
+	printf("\n"); printf("\n"); printf("5:\n");
+	scanf_s("%f", &i);
+	scanf_s("%f", &j);
+	scanf_s("%f", &k);
+	printf("At %.2f megabits per second,a file of %.2f megabytes dowmloads in %.2f seconds\n",
+		i, j, k);
+
+	printf("\n"); printf("\n"); printf("6:\n");
+	printf("xin:\n");
+	scanf_s("%s", name1, 10);
+    printf("min:\n");
+	scanf_s("%s", name2, 10);	
+	printf("1-%-10s.%-10s\n", name1, name2);
+	printf("2-%-10d%-10d\n", sizeof(name1), sizeof(name2));
+	printf("3-%10s.%10s\n", name1, name2);
+	printf("4-%10d%10d\n", sizeof(name1), sizeof(name2));
+
+	printf("\n"); printf("\n"); printf("8:\n");
+	printf("lenth:\n");
+	scanf_s("%f", &i);
+	printf("oil:\n");
+	scanf_s("%f", &j);
+	k = j / i;
+	printf("%.1f", k);
+*/	
+	return 0;
+}
